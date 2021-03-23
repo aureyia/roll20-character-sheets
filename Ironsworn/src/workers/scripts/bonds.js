e23c376dca5ca0962469e8578b55a7662e25454c
LeeRhodes <leerhodes@protonmail.com>
2021-03-23 15:15:29 +0000

    updated bonds work

--- Ironsworn/src/workers/scripts/bonds.js
+++ Ironsworn/src/workers/scripts/bonds.js
@@ -3,3 +3,9 @@ on('change:repeating_bonds:details_button', function(eventinfo) {
     repeating_bonds_details: eventinfo.newValue,
   });
 });
+
+on('change:repeating_bonds:links_button', function(eventinfo) {
+  setAttrs({
+    repeating_bonds_links: eventinfo.newValue,
+  });
+});
